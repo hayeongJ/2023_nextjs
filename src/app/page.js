9bb4c1aee7e23e1ea9903c3c883650f940e1e776
hayeong <hayoungju98@naver.com>
2023-08-30 10:04:12 +0900

    기본 문서 완성

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       Hello, WEB !!
       {/* 해당 이미지를 따로 import 하지 않으면 widh, height를 반드시 지정해야한다. */}
       <p><Image src="/images/zzang_1.png" width={100} height={100} /></p>
-      <p><Image src={img1} /></p>
+      <p><Image src={img1} alt="" width={100} height={100} /></p>
     </>
     
   );
